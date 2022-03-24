4f0194314dad177f2507a63b77471627a5e4ef82
“JazlynL” <lubenjazlyn35@gmail.com>
2022-03-23 23:23:07 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/GoRest1/GoRest1Application.java
@@ -0,0 +1,13 @@
+package com.careerdevs.GoRest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoRest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoRest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/GoRest1/GoRest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.GoRest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoRest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
