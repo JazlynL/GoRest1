dd5aa54361638089642f5a4e1f59eba0d8d9e54d
“JazlynL” <lubenjazlyn35@gmail.com>
2022-03-31 16:16:47 -0400

    completed getting all users on GORESTAPI

--- src/main/java/com/careerdevs/GoRest1/controller/UserController.java
+++ src/main/java/com/careerdevs/GoRest1/controller/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 
 @RestController
@@ -207,16 +209,19 @@ public class UserController {
 
 
 
+
+
     // getting all the pages for user
 
     //url / endpoint GET  http://localhost:4000/api/user/firstpage
-
 
     @GetMapping("/page/{pageNum}")
    public Object getFirstPage(RestTemplate restTemplate,
                               @PathVariable("pageNum") String pageNumber){
         try{
             String url = "https://gorest.co.in/public/v2/users?page="+ pageNumber;
+
+            //
             ResponseEntity<UserModelExample[]> firstPage = restTemplate.getForEntity(url,UserModelExample[].class);
 
 
@@ -236,8 +241,8 @@ public class UserController {
 
             //assigning the object key header
             String totalPages = Objects.requireNonNull( responseHeaders.get("X-Pagination-Pages").get(0));
-           // outputting totalpages in the terminal
 
+           // outputting totalpages in the terminal
             System.out.println("Total pages: "+ totalPages);
 
             return new ResponseEntity<>(firstPageOfUsers,HttpStatus.OK);
@@ -249,4 +254,57 @@ public class UserController {
         }
     }
 
+    //getting all users
+    @GetMapping("/all")
+    public ResponseEntity getAll(RestTemplate restTemplate) {
+        try{
+           //creating an Array List to get all the users.
+            ArrayList<UserModelExample> allUsers = new ArrayList<>();
+            String url = "https://gorest.co.in/public/v2/users";
+
+            /*creating a response entity.
+            Since we are requesting an Array of Users.
+            We are going to be implementing the User Model Array,
+            We will be using get request for entity because we want what's in the headers.
+            We will implement in the parameters the url, and the data type we are using which
+            is  the array class.
+             */
+
+            ResponseEntity<UserModelExample[]> response = restTemplate.getForEntity(url, UserModelExample[].class);
+
+
+            /* we are using addAll to input a collection.
+            We use addAll when working with Arraylists,Arrays is a static class that
+            have all these different methods.
+            Takes an array of any kind and makes it into a list.
+            */
+
+            allUsers.addAll(Arrays.asList(Objects.requireNonNull(response.getBody())));
+
+           //accessing the Integer class and using parse so it can be read.
+            int totalPages = Integer.parseInt(response.getHeaders().get("X-Pagination-Pages").get(0));
+
+            //at this point we will begin iterating through the pages
+
+            for(int i = 2; i <= totalPages;i++){
+                String tempUrl = url+"?page="+i;
+                UserModelExample[] pageData = restTemplate.getForObject(tempUrl, UserModelExample[].class);
+                allUsers.addAll(Arrays.asList(Objects.requireNonNull(pageData)));
+            }
+            System.out.println(allUsers);
+
+            return new ResponseEntity(allUsers,HttpStatus.OK);
+
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
+
+            // will return a message, status, or body
+
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+    }
 }
